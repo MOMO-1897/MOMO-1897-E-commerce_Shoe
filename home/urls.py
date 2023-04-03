cbcedb9a8f02176484ebd3e2b26dfd37fccd40df
MOMO-1897 <rishavtamrakar07@gmail.com>
2023-04-03 14:42:56 +0545

    all pages can render

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-   path('', HomeView.as_view(), name='home')
+   path('', Index.as_view(), name='index'),
+   path('index.html', Index.as_view(), name='index'),
+   path('about.html', About.as_view(), name='about'),
+   path('shop.html', Shop.as_view(), name='shop'),
+   path('contact.html', Contact.as_view(), name='shop')
 ]
--- home/views.py
+++ home/views.py
@@ -7,7 +7,24 @@ class Base(View):
     views = {}
 
 
-class HomeView(Base):
+class Index(Base):
 
     def get(self, request):
         return render(request, 'index.html')
+
+
+class About(Base):
+
+    def get(self, request):
+        return render(request, 'about.html')
+
+class Shop(Base):
+
+    def get(self, request):
+        return render(request, 'shop.html')
+
+class Contact(Base):
+
+    def get(self, request):
+        return render(request, 'contact.html')
+
