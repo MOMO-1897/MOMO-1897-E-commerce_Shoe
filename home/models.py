5a283852a51fcd3704f44877271efaef7b257f69
MOMO-1897 <rishavtamrakar07@gmail.com>
2023-04-06 10:23:17 +0545

    new commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class categories(models.Model):
+    name = models.CharField(max_length=300)
+    pic = models.ImageField(upload_to='images')
+    slug = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
