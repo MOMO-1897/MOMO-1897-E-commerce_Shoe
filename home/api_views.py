281ac2e9b4447924eaf86978c8991bc71445a49b
MOMO-1897 <rishavtamrakar07@gmail.com>
2023-04-29 21:34:17 +0545

    Updated api

--- home/api_views.py
+++ home/api_views.py
@@ -5,6 +5,9 @@ from .serializers import *
 import django_filters.rest_framework
 from .serializers import *
 from rest_framework import generics
+from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework import status
 
 from django.http import Http404
 from rest_framework.views import APIView
@@ -28,4 +31,37 @@ class BrandsViewSet(viewsets.ModelViewSet):
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'slug']
+    ordering_fields = ['name', 'id']
+    filterset_fields = ['name', 'subcategory']
+
+
+class ProductDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
